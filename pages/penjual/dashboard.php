9189be7e431e44b7f1651760bd49d576f5014584
abdmmar <opowae909@gmail.com>
2021-01-10 11:36:06 +0700

    Add favicon in pembeli and penjual page

--- pages/penjual/dashboard.php
+++ pages/penjual/dashboard.php
@@ -28,7 +28,7 @@ if (isset($_GET['p'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title ?></title>
-  <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+  <link rel="shortcut icon" href="../../logo.png" type="image/x-icon" />
   <link rel="stylesheet" href="../../style/style.css">
 </head>
 
